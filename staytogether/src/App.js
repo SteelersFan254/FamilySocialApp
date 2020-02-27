2ee69e39a9f616eaf778021ca100a642313fc6ca
Ryan Borja <rcborja.school@gmail.com>
2020-02-27 00:04:42 -0600

    made pages folder

--- staytogether/src/App.js
+++ staytogether/src/App.js
@@ -1,25 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+import Contacts from "./pages/Contacts";
+import Maps from "./pages/Map";
+import Tree from "./pages/Tree";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>div className="App">
+      <div>
+        <Navbar />
+        <Route exact path="/" component={Login} />
+        <Route exact path="/home" component={Home} />
+        <Route exact path="/contacts" component={Contacts} />
+        <Route exact path="/map" component={Maps} />
+        <Route exact path="/tree" component={Tree} />
+
+      </div>
+    </Router> <
   );
 }
 
