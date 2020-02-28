835ff350bc186ab672bbe99241b1960f77f60692
Ryan Borja <rcborja.school@gmail.com>
2020-02-28 10:04:59 -0600

    switched to mysql

--- staytogether/src/components/Card/index.js
+++ staytogether/src/components/Card/index.js
@@ -1,10 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style.css"
+// import Modal from "../Modal"
 
-function Card () {
+// const [show, setShow] = useState(false);
+
+// const handleClose = () => setShow(false);
+// const handleShow = () => setShow(true);
+
+function Card() {
     return (
         <div className="card">
             Name
+            {/* <button variant="primary" onClick={handleShow}>
+                Launch demo modal
+            </button> */}
+
+            {/* <Modal show={show} onHide={handleClose}>
+                <header closeButton>
+                    <p>Modal heading</p>
+                </header>
+                <p>Woohoo, you're reading this text in a modal!</p>
+                <button variant="secondary" onClick={handleClose}>
+                    Close
+                </button>
+            </Modal> */}
         </div>
     )
 };
---
+++ staytogether/src/components/CreateEvent/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./style.css"
+function CreateEvent() {
+
+    return (
+        <div style={{width:"100%"}}>
+            <div class="card" style={{width: "max-width"}}>
+                <div class="card-body">
+                    <p>Create Event</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default CreateEvent;
