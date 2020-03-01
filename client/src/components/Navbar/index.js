f9bc841d678cb0ba62493852c3ef4420356ad667
Ryan Borja <rcborja.school@gmail.com>
2020-03-01 16:53:30 -0600

    made profile page

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -5,31 +5,49 @@ import "./style.css";
 
 function Navbar() {
     return (
-        <div className="navbar navbarDiv">
-            <Link
-                to="/home"
-                className={window.location.pathname === "/home"}
-            >
-                Home
-      </Link>
-      <Link
-                to="/contacts"
-                className={window.location.pathname === "/contacts"}
-            >
-                Contacts
-      </Link>
-      <Link
-                to="/map"
-                className={window.location.pathname === "/map"}
-            >
-                Map
-      </Link>
-      <Link
-                to="/tree"
-                className={window.location.pathname === "/tree"}
-            >
-                Tree
-      </Link>
+        <div>
+            <div className="navbar navbarDiv">
+                <div className="linkBox">
+                    <Link
+                        to="/home"
+                        className={window.location.pathname === "/home"}
+                    >
+                        Home
+                    </Link>
+                    <Link
+                        to="/contacts"
+                        className={window.location.pathname === "/contacts"}
+                    >
+                        Contacts
+                    </Link>
+                    <Link
+                        to="/map"
+                        className={window.location.pathname === "/map"}
+                    >
+                        Map
+                    </Link>
+                    <Link
+                        to="/tree"
+                        className={window.location.pathname === "/tree"}
+                    >
+                        Tree
+                    </Link>
+                </div>
+                <div className="otherNavBox">
+                <Link
+                        to="/profile"
+                        className={window.location.pathname === "/profile"}
+                    >
+                        Profile
+                    </Link>
+                    <Link
+                        to="/login"
+                        className={window.location.pathname === "/login"}
+                    >
+                        Logout
+                    </Link>
+                </div>
+            </div>
         </div>
 
     );
