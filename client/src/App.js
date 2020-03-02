58d94463bdc6432d8d45abf0ae85d9650e184329
Caleb Vincent <vincent_806@yahoo.com>
2020-03-02 10:08:50 -0600

    Committing before beginning group work, no changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,5 @@
-
-import axios from "axios";
-import API from "./utils/API";
-import React, { useEffect, useState} from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
-
 import Login from "./pages/Login";
 import Home from "./pages/Home";
 import Contacts from "./pages/Contacts";
@@ -13,63 +9,10 @@ import Profile from "./pages/Profile";
 import Navbar from "./components/Navbar";
 //import LoginNavbar from "./components/LoginNavbar";
 import './App.css';
-
-
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [privateInfo, setPrivateInfo] = useState({});
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
-  function handleSignupSubmit(event){
-    event.preventDefault();
-    console.log("button is clicking")
-    API.signup(email, password).then(response => console.log(response.data))
-  }
-
-  function handleSigninSubmit(event){
-    event.preventDefault();
-    API.signin(email, password).then(response => {
-      setIsLoggedIn(true);
-      axios.get("/api/secret/number").then(response => {
-        setPrivateInfo({ secret: response.data})
-      })
-    })
-  }
-  function handleSignoutSubmit(event){
-    event.preventDefault();
-    API.signout().then(response => {
-      setIsLoggedIn(false);
-    })
-  }
-
-  function renderPrivateSection(){
-    if(isLoggedIn){
-      return <div>
-        <h1>{privateInfo.secret}is authenticated</h1>
-      </div>
-    }
-  }
-
-
   return (
-    
     <Router>
-      <div>    <div className="App">
-      <h1>Signup</h1>
       <div>
-        <label htmlFor="email">Email: </label>
-        <input name="email" type="text" value={email} onChange={(event) => { setEmail(event.target.value)}}/><br/>
-        <label htmlFor="email">Password: </label>
-        <input name="password" type="password" value={password} onChange={(event) => { setPassword(event.target.value)}}/> <br/>
-        <button onClick={handleSignupSubmit}>Sign Up!</button>
-        <button onClick={handleSigninSubmit}>Sign In!</button>
-        <button onClick={handleSignoutSubmit}>Sign Out!</button>
-      </div>
-      {
-        renderPrivateSection()
-      }      
-    </div>
         {/* <Route exact path="/" component={LoginNavbar} /> */}
         <Navbar />
         <Route exact path="/" component={Login} />
@@ -81,7 +24,5 @@ function App() {
       </div>
     </Router>
   )
-  
 };
-
 export default App;
