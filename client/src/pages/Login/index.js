680b2465237a49ad35fbead912d06de2728d19f9
Ryan Borja <rcborja.school@gmail.com>
2020-03-03 11:28:35 -0600

    sign in working

--- client/src/pages/Login/index.js
+++ client/src/pages/Login/index.js
@@ -1,32 +1,42 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { Link } from "react-router-dom";
 import "./style.css";
+import API from "../../utils/API.js"
+import axios from "axios";
+
 
 
 
 function Login() {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
-    function handleButtonSubmit() {
-        console.log(email, password);
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+
+    function handleButtonSubmit(event) {
+        event.preventDefault();
+        API.login(email, password).then(response => {
+            setIsLoggedIn(true)
+            console.log("logged in")
+        })
     }
 
     return (
         <div>
-                <div className="loginBox">
-                    <div className="loginBoxTitle">
-                        <span style={{fontSize: "48px"}}>SIGN IN</span>
-                    </div>
-                    <input name="email" className="loginEmailInput" type="text" placeholder="Email" value={email} onChange={(event) => { setEmail(event.target.value)}}/>
-                    <input name="password" className="loginPasswordInput" placeholder="Password" type="password" value={password} onChange={(event) => { setPassword(event.target.value)}}/>
-                    <input name="submit" className="loginSubmitButton" type="button" onClick={{handleButtonSubmit}}></input>
+            <div className="loginBox">
+                <div className="loginBoxTitle">
+                    <span style={{ fontSize: "48px" }}>SIGN IN</span>
                 </div>
-                    <Link
-                        to="/signup"
-                        className={window.location.pathname === "/signup"}
-                    >
-                        <p>SIGNUP</p>
-                    </Link>
+                <input name="email" className="loginEmailInput" type="text" placeholder="Email" value={email} onChange={(event) => { setEmail(event.target.value) }} />
+                <input name="password" className="loginPasswordInput" placeholder="Password" type="password" value={password} onChange={(event) => { setPassword(event.target.value) }} />
+                <button className="loginSubmitButton" onClick={handleButtonSubmit}>SUBMIT</button>>
+                </div>
+            <Link
+                to="/signup"
+                className={window.location.pathname === "/signup"}
+            >
+                <p>SIGNUP</p>
+            </Link>
 
         </div>
     )
